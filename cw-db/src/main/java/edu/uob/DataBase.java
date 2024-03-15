598a5293943edc98022e020721c63b61336b2337
sparklefoa1 <caizixuan21@163.com>
2024-03-15 01:00:38 +0000

    add Parser & ParserTest

--- cw-db/src/main/java/edu/uob/DataBase.java
+++ cw-db/src/main/java/edu/uob/DataBase.java
@@ -27,23 +27,4 @@ public class DataBase {
             System.out.println("Failed to delete database folder: " + storageFolderPath);
         }
     }
-
-    /* public String DropDataBase(String DBname) {
-        if(DBname.isDirectory()) {
-            // 获取文件夹中的所有文件和子文件夹
-            File[] files = DBname.listFiles();
-            if(files != null) {
-                for (File file : files) {
-                            // 递归删除子文件夹和文件
-                    deleteFolder(file);
-                }
-            }
-        }
-                // 删除文件夹本身
-        if (!folder.delete()) {
-            System.err.println("Failed to delete folder: " + folder.getAbsolutePath());
-        } else {
-            System.out.println("Folder deleted: " + folder.getAbsolutePath());
-        }
-    }*/
 }
---
+++ cw-db/src/main/java/edu/uob/Parser.java
@@ -0,0 +1,47 @@
+package edu.uob;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Parser {
+    String query = "  INSERT  INTO  people   VALUES(  'Simon Lock'  ,35, 'simon@bristol.ac.uk' , 1.8  ) ; ";
+    String[] specialCharacters = {"(",")",",",";"};
+    ArrayList<String> tokens = new ArrayList<String>();
+
+    void setup()
+    {
+        // Remove any whitespace at the beginning and end of the query
+        query = query.trim();
+        // Split the query on single quotes (to separate out query characters from string literals)
+        String[] fragments = query.split("'");
+        for (int i=0; i<fragments.length; i++) {
+            // Every odd fragment is a string literal, so just append it without any alterations
+            if (i%2 != 0) tokens.add("'" + fragments[i] + "'");
+                // If it's not a string literal, it must be query characters (which need further processing)
+            else {
+                // Tokenise the fragments into an array of strings
+                String[] nextBatchOfTokens = tokenise(fragments[i]);
+                // Then add these to the "result" array list (needs a bit of conversion)
+                tokens.addAll(Arrays.asList(nextBatchOfTokens));
+            }
+        }
+        // Finally, loop through the result array list, printing out each token a line at a time
+        for(int i=0; i<tokens.size(); i++) System.out.println(tokens.get(i));
+    }
+
+    String[] tokenise(String input)
+    {
+        // Add in some extra padding spaces around the "special characters"
+        // so we can be sure that they are separated by AT LEAST one space (possibly more)
+        for(int i=0; i<specialCharacters.length ;i++) {
+            input = input.replace(specialCharacters[i], " " + specialCharacters[i] + " ");
+        }
+        // Remove all double spaces (the previous replacements may had added some)
+        // This is "blind" replacement - replacing if they exist, doing nothing if they don't
+        while (input.contains("  ")) input = input.replaceAll("  ", " ");
+        // Again, remove any whitespace from the beginning and end that might have been introduced
+        input = input.trim();
+        // Finally split on the space char (since there will now ALWAYS be a space between tokens)
+        return input.split(" ");
+    }
+}
