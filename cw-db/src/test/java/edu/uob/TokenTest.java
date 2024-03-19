cf4d07ee5dba2f8c2059fe7d8713e1bae53baba3
sparklefoa1 <caizixuan21@163.com>
2024-03-19 18:30:09 +0000

    reconstruct the classes and rename them

---
+++ cw-db/src/test/java/edu/uob/TokenTest.java
@@ -0,0 +1,14 @@
+package edu.uob;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TokenTest {
+    private Token token;
+    @BeforeEach
+    public void setup() {
+        token = new Token();}
+    @Test
+    public void testParser(){
+        token.setup();}
+}
